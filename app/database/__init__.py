37f57d39bd4b5a9a5acef16756dca2856ecde0ff
gabrielbaute <gabrielbaute@gmail.com>
2025-06-28 22:53:41 -0400

    Variable __all__

--- app/database/__init__.py
+++ app/database/__init__.py
@@ -1,2 +1,10 @@
 from app.database.db_settings import db, init_db
 from app.database.signos_model import Signos, Predicciones, Imagenes
+
+__all__ = [
+    "db",
+    "init_db",
+    "Signos",
+    "Predicciones",
+    "Imagnes"
+]
