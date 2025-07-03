1d862c8b5277db94b39bdaf552d2cd35077788dd
anjivervas <anjivervas03@gmail.com>
2025-07-03 15:45:11 -0400

    Refactorizacion de la clase Config

--- app/settings/config.py
+++ app/settings/config.py
@@ -1,6 +1,11 @@
-"""Configuración general de la aplicación."""
+import os
+from dotenv import load_dotenv
 
 class Config:
-    #Configuracion de SQLAlchemy
-    SQLALCHEMY_DATABASE_URI= "sqlite:///database.db"
-    SQLALCHEMY_TRACK_MODIFICATION= False
+    def __init__(self):
+        load_dotenv()
+        self.TELEGRAM_BOT_KEY = os.getenv("TELEGRAM_BOT_KEY")
+        self.LOG_LEVEL = os.getenv("LOG_LEVEL", "INFO")
+        self.URL_BASE = os.getenv("URL_BASE", "https://www.bcv.org.ve")
+        self.APP_NAME = os.getenv("APP_NAME", "HoróscopoBot")
+        self.AUTHOR_NAME = os.getenv("AUTHOR_NAME", "Anjiver")
