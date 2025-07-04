f3c530ec2cd7bceb9f981542573adfe60e8e53c0
gabrielbaute <gabrielbaute@gmail.com>
2025-07-04 09:41:48 -0400

    Rastreo mas detallado en scraper

--- app/core/scraper.py
+++ app/core/scraper.py
@@ -24,7 +24,7 @@ class BCVScraper:
             response.raise_for_status()
             return BeautifulSoup(response.text, "html.parser")
         except requests.RequestException as e:
-            logger.error(f"[BCVScraper] Error obteniendo la página del BCV: {e}")
+            logger.error(f"[{Config().APP_NAME}: Scraper Module] Error obteniendo la página del BCV: {e}")
             return None
 
     def _extraer_divisa_raw(self, divisa: Divisas) -> Optional[str]:
@@ -34,12 +34,12 @@ class BCVScraper:
 
         div_container = self._soup.find("div", {"id": divisa.divisa()})
         if not div_container:
-            logger.error(f"[BCVScraper] No se encontró el contenedor para la divisa {divisa}")
+            logger.error(f"[{Config().APP_NAME}: Scraper Module] No se encontró el contenedor para la divisa {divisa}")
             return None
 
         valor_div = div_container.find("div", {"class": "col-sm-6 col-xs-6 centrado"})
         if not valor_div:
-            logger.error(f"[BCVScraper] No se encontró la tasa para la divisa {divisa}")
+            logger.error(f"[{Config().APP_NAME}: Scraper Module] No se encontró la tasa para la divisa {divisa}")
             return None
 
         return valor_div.get_text(strip=True)
@@ -51,10 +51,10 @@ class BCVScraper:
             return None
 
         try:
-            logger.info(f"Scrapeando divisa: {divisa}")
+            logger.info(f"[{Config().APP_NAME}: Scraper Module] Scrapeando divisa: {divisa}")
             return float(valor_crudo.replace(",", "."))
         except ValueError:
-            logger.error(f"[BCVScraper] Error parseando el valor '{valor_crudo}' para {divisa}")
+            logger.error(f"[{Config().APP_NAME}: Scraper Module] Error parseando el valor '{valor_crudo}' para {divisa}")
             return None
 
     def obtener_todas(self) -> dict[Divisas, float]:
@@ -64,4 +64,5 @@ class BCVScraper:
             tasa = self.obtener_tasa(divisa)
             if tasa is not None:
                 tasas[divisa] = tasa
+        logger.info(f"[{Config().APP_NAME}: Scraper Module] Tasas obtenidas: {tasas}")
         return tasas
